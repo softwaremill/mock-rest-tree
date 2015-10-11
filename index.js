c5c2f9503c9909182d960d32275337fc5e6ab424
Bartosz Andrzejczak <bartosz.andrzejczak@softwaremill.com>
2015-10-11 18:05:54 +0200

    Moves all specific information into options

--- index.js
+++ index.js
@@ -4,69 +4,122 @@ var express = require('express');
 
 function MockServer(options) {
   var app = express();
+  var mockServer = this;
 
-  this.options = {
-    data: './fileTree.json',
+  mockServer.options = {
+    // Path to JSON file containing mock server data
+    data: './data.json',
+
+    // HTTP path of resource containing single tree node
+    elementPath: '/node',
+
+    // HTTP path of resource containing tree node with it's immediate children
+    collectionPath: '/children',
+
+    // Name of JSON field which will be used for tree navigation
+    // e.g. if idFieldName === 'id', /node/123 will retrieve
+    // node with id field set to 123
+    idFieldName: 'id',
+
+    // Name of JSON field which will be used to get node's children
+    // e.g. if childrenFieldName === 'children', /children will
+    // return an empty root element, with all it's immediate children
+    // stored in children field
+    childrenFieldName: 'children',
+
+    // If set to true, the server will automatically add path field to each
+    // served element. The path field will contain values of fields named in
+    // idFieldName from the root element up to served element i.e. for
+    // /node/123/456, the path will be '/123/456'
+    includePath: true,
+
+    // The port on which the API will be served upon its start
     port: 3000
   };
-  _.assign(this.options, options || {});
 
-  var fileTree = require(this.options.data);
+  _.assign(mockServer.options, options || {});
 
-  app.get(["/folder", "/folder*"], function (req, res) {
-    var pathSegments = extractPathSegments(req);
-    var folder = findFile(pathSegments);
-    folder.items = _.map(folder.items, function (element) {
-      element.items = undefined;
-      var folderPath = _.clone(pathSegments);
-      folderPath.push(element.title);
-      return setPath(element, folderPath);
+  var data = mockServer.options.dataObject || require(mockServer.options.data);
+
+  function handleCollectionRequest(req, res) {
+    var pathSegments = extractPathSegments(req, mockServer.options.collectionPath);
+    var node = findNode(pathSegments);
+    node[mockServer.options.childrenFieldName] = _.map(node[mockServer.options.childrenFieldName], function (child) {
+      child[mockServer.options.childrenFieldName] = undefined;
+      return setPathIncludingElementIfEnabled(child, pathSegments);
     });
-    res.send(JSON.stringify(folder));
-  });
-
-  app.get(["/file", "/file*"], function (req, res) {
-    var pathSegments = extractPathSegments(req);
-    var fileDetails = findFile(pathSegments);
-    fileDetails.items = undefined;
-    res.send(JSON.stringify(fileDetails));
-  });
-
-  function findFile(pathSegments) {
-    return setPath(
-      _.cloneDeep(
-        _.reduce(pathSegments, function (file, segment) {
-          return _.find(_.result(file, 'items'), 'title', segment);
-        }, {items: fileTree})
-      ),
-      pathSegments
-    );
+    res.send(JSON.stringify(node));
   }
 
-  function extractPathSegments(req) {
-    var pathSegments = decodeURI(req.originalUrl).substring(1).split('/');
-    return _.filter(_.drop(pathSegments, 1), function (segment) {
+  function handleElementRequest(req, res) {
+    var pathSegments = extractPathSegments(req, mockServer.options.elementPath);
+    var node = findNode(pathSegments);
+    node[mockServer.options.childrenFieldName] = undefined;
+    res.send(JSON.stringify(node));
+  }
+
+  function extractPathSegments(req, resourceRootPath) {
+    var pathSegments = decodeURI(req.originalUrl).substring(resourceRootPath.length).split('/');
+    return _.filter(pathSegments, function (segment) {
       return segment.length > 0;
     });
   }
 
-  function setPath(document, pathSegments) {
-    document.path = encodeURI('/' + pathSegments.join('/'));
-    return document;
+  function findNode(pathSegments) {
+    var initialObject = {};
+    initialObject[mockServer.options.childrenFieldName] = data;
+    var node = _.cloneDeep(
+      _.reduce(pathSegments, function (node, segment) {
+        return _.find(
+          _.result(
+            node,
+            mockServer.options.childrenFieldName
+          ),
+          mockServer.options.idFieldName,
+          segment
+        );
+      }, initialObject)
+    );
+    return setPathIfEnabled(node, pathSegments);
   }
 
-  app.put(["/file", "/file*"], function (req, res) {
-    res.send("ok");
-  });
+  function setPathIncludingElementIfEnabled(element, pathSegments) {
+    var path = _.clone(pathSegments);
+    path.push(element.title);
+    return setPathIfEnabled(element, path);
+  }
 
-  var server = app.listen(this.options.port, function () {
+  function setPathIfEnabled(element, pathSegments) {
+    if(mockServer.options.includePath) {
+      element.path = encodeURI('/' + pathSegments.join('/'));
+    }
+    return element;
+  }
+
+  app.get(
+    [
+      mockServer.options.collectionPath,
+      mockServer.options.collectionPath + "*"
+    ],
+    handleCollectionRequest
+  );
+
+  app.get(
+    [
+      mockServer.options.elementPath,
+      mockServer.options.elementPath + "*"
+    ],
+    handleElementRequest
+  );
+
+  var server = app.listen(mockServer.options.port, function () {
     var host = server.address().address;
     var port = server.address().port;
 
     console.log('Mock data server is listening at http://%s:%s', host, port);
   });
 
-  this.close = function () {
+  mockServer.close = function () {
     server.close();
   }
 }
